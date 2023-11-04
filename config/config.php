6d463d07b8448b5bc14c9f0c87a247d617a0c923
deej-0101 <diane.joy.d.n@gmail.com>
2023-11-04 22:08:47 +0800

    added table office

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
     define('ROOT_URL' , 'http://localhost/myphp/aaa/bootstrap/');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'helloadmin');
-    define('DB_NAME', 'recordsapp_db');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'recordsapp');
 
 
 
--- office.php
+++ office.php
@@ -19,6 +19,27 @@
 </head>
 
 <body>
+<?php
+require('config/config.php');
+require('config/db.php');
+
+// create query
+$query = 'SELECT * FROM office ORDER BY name';
+
+// get the result
+$result = mysqli_query($conn, $query);
+
+// fetch the data
+$offices = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result 
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+?>
+
     <div class="wrapper">
         <div class="sidebar" data-image="../assets/img/sidebar-5.jpg">
             <!--
@@ -128,6 +149,77 @@
             <div class="content">
                 <div class="container-fluid">
                     <div class="section">
+                    </div>
+                    <div class="row">
+                    <div class="col-md-12">
+                            <div class="card strpied-tabled-with-hover">
+                                <div class="card-header ">
+                                    <h4 class="card-title">Striped Table with Hover</h4>
+                                    <p class="card-category">Here is a subtitle for this table</p>
+                                </div>
+                                <div class="card-body table-full-width table-responsive">
+                                    <table class="table table-hover table-striped">
+                                        <thead>
+                                            <th>Name</th>
+                                            <th>Contact Number</th>
+                                            <th>Email</th>
+                                            <th>Address</th>
+                                            <th>City</th>
+                                            <th>Country</th>
+                                            <th>Postal</th>
+                                        </thead>
+                                        <tbody>
+                                            <?php foreach($offices as $office) : ?>
+                                            <tr>
+                                                <td><?php echo $office['name']; ?></td>
+                                                <td><?php echo $office['contactnum']; ?></td>
+                                                <td><?php echo $office['email']; ?></td>
+                                                <td><?php echo $office['address']; ?></td>
+                                                <td><?php echo $office['city']; ?></td>
+                                                <td><?php echo $office['country']; ?></td>
+                                                <td><?php echo $office['postal']; ?></td>
+                                            </tr>
+                                            <?php endforeach ?>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Minerva Hooper</td>
+                                                <td>$23,789</td>
+                                                <td>Curaçao</td>
+                                                <td>Sinaai-Waas</td>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td>Sage Rodriguez</td>
+                                                <td>$56,142</td>
+                                                <td>Netherlands</td>
+                                                <td>Baileux</td>
+                                            </tr>
+                                            <tr>
+                                                <td>4</td>
+                                                <td>Philip Chaney</td>
+                                                <td>$38,735</td>
+                                                <td>Korea, South</td>
+                                                <td>Overland Park</td>
+                                            </tr>
+                                            <tr>
+                                                <td>5</td>
+                                                <td>Doris Greene</td>
+                                                <td>$63,542</td>
+                                                <td>Malawi</td>
+                                                <td>Feldkirchen in Kärnten</td>
+                                            </tr>
+                                            <tr>
+                                                <td>6</td>
+                                                <td>Mason Porter</td>
+                                                <td>$78,615</td>
+                                                <td>Chile</td>
+                                                <td>Gloucester</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -249,20 +341,20 @@
  -->
 </body>
 <!--   Core JS Files   -->
-<script src="../assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
-<script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/core/bootstrap.min.js" type="text/javascript"></script>
 <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-<script src="../assets/js/plugins/bootstrap-switch.js"></script>
+<script src="assets/js/plugins/bootstrap-switch.js"></script>
 <!--  Google Maps Plugin    -->
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
 <!--  Chartist Plugin  -->
-<script src="../assets/js/plugins/chartist.min.js"></script>
+<script src="assets/js/plugins/chartist.min.js"></script>
 <!--  Notifications Plugin    -->
-<script src="../assets/js/plugins/bootstrap-notify.js"></script>
+<script src="assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Light Bootstrap Dashboard: scripts for the example pages etc -->
-<script src="../assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
+<script src="assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
 <!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
-<script src="../assets/js/demo.js"></script>
+<script src="assets/js/demo.js"></script>
 
 </html>
